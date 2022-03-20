aa6e847dc9fe62d788e141de966d9748aad2029f
Philipp Fuerholz <fuerholz@gmx.ch>
2022-03-20 22:12:04 +0100

    gain stage
    simple saturating gain stage added to audio components

---
+++ Inc/audio/gainstage.h
@@ -0,0 +1,15 @@
+#ifndef _GAINSTAGE_H_
+#define _GAINSTAGE_H_
+#include <stdint.h>
+
+typedef struct
+{
+    int16_t gain; // 512 is unity gain
+    /* data */
+} gainStageData;
+
+void initGainstage(gainStageData*data);
+
+int16_t gainStageProcessSample(int16_t sampleIn,gainStageData*data);
+
+#endif
---
+++ Src/common/audio/gainstage.c
@@ -0,0 +1,28 @@
+#include "audio/gainstage.h"
+
+
+void initGainstage(gainStageData*data)
+{
+    data->gain=512;
+}
+
+int16_t gainStageProcessSample(int16_t sampleIn,gainStageData*data)
+{
+    int16_t sampleOut;
+    int32_t sampleWord = (int32_t)sampleIn;
+    sampleWord = sampleWord* data->gain;
+    sampleWord >>= 9;
+    if( sampleWord < -32767)
+    {
+        sampleOut = -32767;
+    }
+    else if (sampleWord > 32767)
+    {
+        sampleOut = 32767;
+    }
+    else
+    {
+        sampleOut=(int16_t)(sampleWord & 0xFFFF);
+    }
+    return sampleOut;
+}
