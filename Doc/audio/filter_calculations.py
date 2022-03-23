5aeb718cd57184c6e52c1d58d61108f07d911f28
Philipp Fuerholz <fuerholz@gmx.ch>
2022-03-23 08:33:36 +0100

    improved filter computation script

--- Doc/audio/filter_calculations.py
+++ Doc/audio/filter_calculations.py
@@ -3,6 +3,7 @@ import scipy.signal as signal
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import math
+import cmath
 
 
 def int32(x):
@@ -13,38 +14,41 @@ def int32(x):
 
 
 class AudioFilter:
-    def __init__(self,a,b):
+    def __init__(self,a,b,do_overflow=True,bit_res=16):
         self.a=a
         self.b=b
         self.w=[int(0),int(0),int(0)]
+        self.do_overflow=do_overflow
+        self.bit_res = bit_res
 
     def apply(self, sample):
         out = 0
-        self.w[0] = int32(int(sample) - (int32(self.a[0]*self.w[1]) >> 15) - (int32(self.a[1]*self.w[2]) >> 15))
-        out = int32(int32((int32(self.b[0]*self.w[0])>> 15)) + int32((int32(self.b[1]*self.w[1]) >> 15)) + int32((int32(self.b[2]*self.w[2]) >> 15) ))
+        if self.do_overflow is True:
+            self.w[0] = int32(int(sample) - (int32(self.a[0]*self.w[1]) >> (self.bit_res-1)) - (int32(self.a[1]*self.w[2]) >> (self.bit_res-1)))
+            out = int32(int32((int32(self.b[0]*self.w[0])>> (self.bit_res-1))) + int32((int32(self.b[1]*self.w[1]) >> (self.bit_res-1))) + int32((int32(self.b[2]*self.w[2]) >> (self.bit_res-1)) ))
+        else:
+            self.w[0] = (int(sample) - ((self.a[0]*self.w[1]) >> (self.bit_res-1)) - ((self.a[1]*self.w[2]) >> (self.bit_res-1)))
+            out = ((((self.b[0]*self.w[0])>> (self.bit_res-1))) + (((self.b[1]*self.w[1]) >> (self.bit_res-1))) + (((self.b[2]*self.w[2]) >> (self.bit_res-1)) ))
         self.w[2]=self.w[1]
         self.w[1]=self.w[0]
         return out
 
-def compute_td_energy_fraction(bd_orig,ad_orig,sample_size=16,scaling=1.0):
-    bd = bd_orig * scaling
-    ad = ad_orig * scaling
+def compute_td_energy_fraction(bd_orig,ad_orig,do_overflow,sample_size=16):
+    bd = bd_orig
+    ad = ad_orig
     bvals = np.array(bd * ((1 << (sample_size - 1)) - 1)).astype(int)
     avals = np.array(ad[1:] * ((1 << (sample_size - 1)) - 1)).astype(int)
-    testFilter = AudioFilter(avals, bvals)
+    testFilter = AudioFilter(avals, bvals,do_overflow=do_overflow,bit_res=sample_size)
     tdOutput = []
-    max_ampl = (1 << (sample_size-1)) -1
     for c in range(1024):
         if c == 0:
-            inputVal = max_ampl
+            inputVal = 32767
         else:
             inputVal = 0
         tdOutput.append(testFilter.apply(inputVal))
-    energy = np.sqrt(np.sum(np.square(tdOutput))) * scaling
-    if scaling > 0.000001:
-        return energy/max_ampl/scaling
-    else:
-        return 0.
+    energy = np.sqrt(np.sum(np.square(tdOutput)))
+    return energy/32767
+
 
 def get_phase_increments(f_sampling=48000,oversampling=2,bitres=32):
     notes = range(128)
@@ -74,69 +78,59 @@ def get_sine_table():
     return sinetable
 
 
-def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,scaling=None,type="cheby2",ftype="lowpass"):
+def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,do_overflow=True,sample_size=16,scaling=None,type="cheby2",ftype="lowpass"):
     if fc is None:
         fc = fs/2
-    order = 2
-    sample_size = 16
-    auto_scaling = scaling is None
+    if ftype == "highpass" or ftype=="lowpass":
+        order = 2
+    else:
+        order = 1
+    auto_sample_size = sample_size is None
     if type == "cheby2":
         sos = signal.cheby2(order,rs, fc, analog=False, btype=ftype, output='sos', fs=fs)
+        ba = signal.butter(order, fc, analog=False, btype=ftype, output='ba', fs=fs)
     elif type == "cheby1":
         sos = signal.cheby1(order, rs, fc, analog=False, btype=ftype, output='sos', fs=fs)
+        ba = signal.butter(order, fc, analog=False, btype=ftype, output='ba', fs=fs)
     elif type == "butter":
         sos = signal.butter(order, fc, analog=False, btype=ftype, output='sos', fs=fs)
+        ba = signal.butter(order, fc, analog=False, btype=ftype, output='ba', fs=fs)
     else:
         sos= [[1.,0.,0.], [1.,0.,0.]]
 
     cnt=0
     sos_returned = []
     for second_order_filter in sos:
-        bd_orig = second_order_filter[:3]
-        ad_orig = second_order_filter[3:]
+        bd = second_order_filter[:3]
+        ad = second_order_filter[3:]
 
-        if auto_scaling is True:
-            is_done = False
-            scaling = 1.
-            scaling_low = 0.
-            scaling_eps=0.0001
-            while is_done is False:
-                energy_fraction = compute_td_energy_fraction(bd_orig,ad_orig,sample_size,scaling)
-                energy_fraction_l = compute_td_energy_fraction(bd_orig,ad_orig,sample_size,scaling_low)
-                print("upper scaling: {:.4f}, energy fraction: {:.5f}, lower scaling factor: {:.4f}, energy fraction: {:.5f}".format(scaling,energy_fraction,scaling_low,energy_fraction_l))
-                if (energy_fraction > 1.0 and energy_fraction_l < 1.0):
-                    scaling = scaling +0.5*(scaling_low - scaling)
-                elif energy_fraction < 1.0 and energy_fraction_l < 1.0:
-                    scaling_old =scaling
-                    scaling = scaling + (scaling-scaling_low)
-                    scaling_low = scaling_old
-                    if scaling > 1.0:
-                        scaling=1.0
-                        is_done = True
-                elif energy_fraction < 1.0 and energy_fraction_l > 1.0:
-                    scaling = scaling + 0.5 * (scaling - scaling_low)
-                elif energy_fraction > 1.0 and energy_fraction_l > 1.0:
-                    scaling_old = scaling_low
-                    scaling_low = scaling_low + (scaling - scaling_low)
-                    scaling = scaling_old
-                    if scaling_low < 0:
-                        scaling_low = 0
-                if (scaling - scaling_low) < scaling_eps:
-                    is_done = True
-                    bd = bd_orig * scaling_low
-                    ad = ad_orig * scaling_low
-                    bvals = np.array(bd * ((1 << (sample_size - 1)) - 1)).astype(int)
-                    avals = np.array(ad[1:] * ((1 << (sample_size - 1)) - 1)).astype(int)
-                    scaling = scaling_low
-        else:
-            bd = bd_orig * scaling
-            ad = ad_orig * scaling
-            bvals = np.array(bd * ((1 << (sample_size - 1)) - 1)).astype(int)
-            avals = np.array(ad[1:] * ((1 << (sample_size - 1)) - 1)).astype(int)
+        if auto_sample_size is True:
+            sample_size_dyn = 16
+            max_val = ((1 << (sample_size_dyn - 1)) - 1)
+            bvals = np.array(bd * max_val).astype(int)
+            avals = np.array(ad[1:] * max_val).astype(int)
+            done = False
+            while done is False:
+                energy_fraction = compute_td_energy_fraction(bd,ad,True,sample_size_dyn)
+                done = bool(energy_fraction < 1.1)
+                if done is False:
+                    sample_size_dyn -= 1
+            sample_size = sample_size_dyn
+
+        max_val = ((1 << (sample_size - 1)) - 1)
+
+        bvals = np.array(bd * max_val).astype(int)
+        avals = np.array(ad[1:] * max_val).astype(int)
+        # pole calculation
+        p1 = (-float(avals[0]) + cmath.sqrt(float(avals[0])*float(avals[0]) - 4*float(max_val)*float(avals[1])))/(2.*float(max_val))
+        p2 = (-float(avals[0]) - cmath.sqrt(float(avals[0]) * float(avals[0]) - 4 * float(max_val) * float(avals[1]))) /(2.*float(max_val))
+        print("pole 1 at {:.3f}/{:.3f}, absolute value: {:.3f}".format(np.real(p1),np.imag(p1),abs(p1)))
+        print("pole 2 at {:.3f}/{:.3f}, absolute value: {:.3f}".format(np.real(p2), np.imag(p2), abs(p2)))
+
         if do_plot is True:
             print("B coefficients: {}".format(bvals))
             print("A coefficients: {}".format(avals))
-            print("post gain factor: {:.6f}".format(1./scaling))
+            print("sample size: {}".format(sample_size))
 
             wz, hz = signal.freqz(bd, ad)
             fig = plt.figure(figsize=[7.2,7.2],constrained_layout=True)
@@ -147,7 +141,7 @@ def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,scaling=None,
             axs0.plot(freqs,20.*np.log10(abs(hz)),linewidth=2,color="blue")
             axs0.grid(color="gray",linestyle="--",which="major",axis="both")
             axs0.grid(color="gray", linestyle="--", which="minor", axis="x")
-            axs0.axis([1,fs/2,-60,10])
+            axs0.axis([20,fs/2,-60,10])
             axs0.set_xscale("log")
             axs0.set_title("Frequency Response")
             axs0.set_ylabel("Gain [dB]")
@@ -157,7 +151,7 @@ def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,scaling=None,
             axs1.plot(freqs,np.unwrap(np.arctan2(np.real(hz),np.imag(hz)))*180./np.pi,linewidth=2,color="blue")
             axs1.grid(color="gray",linestyle="--",which="major",axis="both")
             axs1.grid(color="gray", linestyle="--", which="minor", axis="x")
-            axs1.set_xlim([1,fs/2])
+            axs1.set_xlim([20,fs/2])
             axs1.set_xscale("log")
             axs1.set_title("Phase response")
             axs1.set_ylabel("Phase [deg]")
@@ -165,7 +159,7 @@ def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,scaling=None,
 
             # time-domain test
             x_time_domain = np.linspace(0,1./fs*1000,1024)
-            testFilter = AudioFilter(avals,bvals)
+            testFilter = AudioFilter(avals,bvals,do_overflow=do_overflow,bit_res=sample_size)
             tdOutput=[]
             for c in range(1024):
                 if c==0:
@@ -175,7 +169,7 @@ def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,scaling=None,
                 tdOutput.append(testFilter.apply(inputVal))
             axs2=fig.add_subplot(gs[2,0])
             axs2.plot(x_time_domain,tdOutput, ".-r", label="positive pulse")
-            testFilter = AudioFilter(avals,bvals)
+            testFilter = AudioFilter(avals,bvals,do_overflow=do_overflow,bit_res=sample_size)
             tdOutput=[]
             for c in range(1024):
                 if c==0:
@@ -184,15 +178,28 @@ def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,scaling=None,
                     inputVal = 0
                 tdOutput.append(testFilter.apply(inputVal))
             axs2.plot(x_time_domain,tdOutput, ".-g", label="negative pulse")
-            axs2.set_ylim([-33000, 33000])
-            axs2.set_yticks(np.arange(start=-32768,stop=32769,step=8192))
+            axs2.set_ylim([-32768*1.007, 32768*1.007])
+            axs2.set_yticks(np.arange(start=-32768,stop=32768,step=32768/4))
             axs2.set_title("Time Domain response")
             axs2.set_xlabel("Time [ms]")
             axs2.set_ylabel("Ampl. [Int16]")
             axs2.grid(color="gray", linestyle="--", which="both", axis="y")
             axs2.legend(loc="upper right")
 
-            axs3 = fig.add_subplot(gs[:,1])
+            tdOutput=[]
+            for c in range(1024):
+                inputVal = int((np.random.random()-0.5)*max_val*2)
+                tdOutput.append(testFilter.apply(inputVal))
+            axs4 = fig.add_subplot(gs[2,1])
+            axs4.plot(x_time_domain,tdOutput, ".-g", label="negative pulse")
+            axs4.set_ylim([-32768*1.007, 32768*1.007])
+            axs4.set_yticks(np.arange(start=-32768,stop=32768,step=32768/4))
+            axs4.set_title("Noise response")
+            axs4.set_xlabel("Time [ms]")
+            axs4.set_ylabel("Ampl. [Int16]")
+            axs4.grid(color="gray", linestyle="--", which="both", axis="y")
+
+            axs3 = fig.add_subplot(gs[:-1,1])
             if type == "cheby1":
                 filter_type_displ = "Chebychev Type I, "
             elif type == "cheby2":
@@ -206,17 +213,17 @@ def design_and_plot_iir_filter(do_plot=True,rs=20,fc=None,fs=48000,scaling=None,
             fsize=0.08
             axs3.text(0.02,0.97-0*fsize,"Second-Order Section {}/{}".format(cnt,len(sos)))
             axs3.text(0.02,0.97-1*fsize,"Filter Type: \n    " + filter_type_displ)
-            freq_atten_displ = "Filter Cutoff Frequencies and Attenuation: \n    {}Hz".format(fc)
+            freq_atten_displ = "Cutoff / Attenuation: \n    {}Hz".format(fc)
             if type!="butter":
                 freq_atten_displ += ", Attenuation: {}dB".format(rs)
             axs3.text(0.02, 0.97 - 2 * fsize, freq_atten_displ)
             axs3.text(0.02,0.97-3*fsize,"B coefficients: \n    {}".format(bvals))
             axs3.text(0.02,0.97-4*fsize,"A coefficients: \n    {}".format(avals))
-            axs3.text(0.02, 0.97-5*fsize, "post gain factor: \n    {:.6f}".format(1./scaling))
+            axs3.text(0.02, 0.97-5*fsize, "bit resolution: \n    {}".format(sample_size))
             axs3.tick_params(bottom=False, labelbottom=False, labelleft=False, left=False)
             axs3.set_ylim([0,1])
             plt.show()
-        sos_returned.append({"b": bvals, "a": avals, "gain_coeff": 1./scaling})
+        sos_returned.append({"b": bvals, "a": avals, "sample_bit_size": sample_size})
     return sos_returned
 
 def design_and_plot_oversampling_lowpass_cheby(do_plot=False,to_integer=True,rs=20,oversampling=2,fc=None):
@@ -341,7 +348,13 @@ if __name__ == "__main__":
     oversampling = 4
     phaseincr_bitsize=32
     rs=3
-    design_and_plot_iir_filter(True,rs=10,fc=170,type="cheby2",ftype="highpass")
+    #design_and_plot_iir_filter(True,rs=10,fc=170,type="cheby2",ftype="highpass")
+    #design_and_plot_iir_filter(True, rs=10, fc=6000, do_overflow=False, type="butter", ftype="lowpass")
+    #design_and_plot_iir_filter(True, rs=10, fc=300, do_overflow=True,sample_size=12,scaling=1.0, type="cheby1", ftype="highpass")
+
+    #design_and_plot_iir_filter(True, rs=10, fc=170, do_overflow=False,sample_size=16, type="butter",ftype="highpass")
+    design_and_plot_iir_filter(True, rs=10, fc=[100,700],do_overflow=False,sample_size=16, type="cheby1", ftype="bandstop")
+
     #design_and_plot_iir_filter(True, to_integer=True, rs=10, fc=6000, scaling=1.0, type="butter", ftype="lowpass")
     #design_and_plot_iir_filter(True, to_integer=True, rs=10, fc=[400, 1700], scaling=0.15, type="cheby2", ftype="bandstop")
     #design_oversampling_comb_lp_filter(rs,oversampling,sample_rate)
