2a7ae815492139c2819293f05cb0ee27a8267b19
Philipp Fuerholz <fuerholz@gmx.ch>
2022-03-23 08:31:45 +0100

    added hi-res mode (64bit intermediate)

--- Inc/audio/secondOrderIirFilter.h
+++ Inc/audio/secondOrderIirFilter.h
@@ -6,9 +6,11 @@ typedef struct
     int32_t w[3];
     int32_t coeffA[2];
     int32_t coeffB[3];
+    uint8_t bitRes;
 } SecondOrderIirFilterType;
 
 void initSecondOrderIirFilter(SecondOrderIirFilterType* data);
 
 int16_t secondOrderIirFilterProcessSample(int16_t sampleIn,SecondOrderIirFilterType*data);
+int16_t secondOrderIirFilterProcessSampleHiRes(int16_t sampleIn,SecondOrderIirFilterType*data);
 #endif
--- Src/common/audio/secondOrderIirFilter.c
+++ Src/common/audio/secondOrderIirFilter.c
@@ -5,24 +5,40 @@ void initSecondOrderIirFilter(SecondOrderIirFilterType* data)
     data->w[0]=0;
     data->w[1]=0;
     data->w[2]=0;
-    data->coeffB[0]=(1<<15);
-    data->coeffB[1]=0;
-    data->coeffB[2]=0;
-    data->coeffA[0]=0;
-    data->coeffA[1]=0;
+}
+
+int16_t secondOrderIirFilterProcessSampleHiRes(int16_t sampleIn,SecondOrderIirFilterType*data)
+{
+    int32_t out;
+    int64_t qprod;
+    qprod = sampleIn;
+    qprod -= ((int64_t)data->coeffA[0]*(int64_t)data->w[1]) >> 15;
+    qprod -= ((int64_t)data->coeffA[1]*(int64_t)data->w[2]) >> 15;
+    data->w[0] = (int32_t)qprod;
+
+    qprod = ((int64_t)data->coeffB[0]*(int64_t)data->w[0]) >> 15;
+    qprod += ((int64_t)data->coeffB[1]*(int64_t)data->w[1]) >> 15;
+    qprod += ((int64_t)data->coeffB[2]*(int64_t)data->w[2]) >> 15;
+    out = (int16_t)qprod;
+    data->w[2] = data->w[1];
+    data->w[1] = data->w[0];
+
+    return (int16_t)(out & 0xFFFF);
 }
 
 int16_t secondOrderIirFilterProcessSample(int16_t sampleIn,SecondOrderIirFilterType*data)
 {
     int32_t out;
-    int32_t a_coeff1, a_coeff2;
+    sampleIn >>= (16 - data->bitRes);
     data->w[0] = (int32_t)sampleIn;
-    data->w[0] -= (((int32_t)data->coeffA[0]*(int32_t)data->w[1]) >> 15);
-    data->w[0] -= (((int32_t)data->coeffA[1]*(int32_t)data->w[2]) >> 15);
+    data->w[0] -= (((int32_t)data->coeffA[0]*(int32_t)data->w[1]) >> (data->bitRes -1));
+    data->w[0] -= (((int32_t)data->coeffA[1]*(int32_t)data->w[2]) >> (data->bitRes -1));
 
-    out = (((int32_t)data->coeffB[0]*(int32_t)data->w[0]) >> 15) + (((int32_t)data->coeffB[1]*(int32_t)data->w[1]) >> 15) + (((int32_t)data->coeffB[2]*(int32_t)data->w[2]) >> 15);
+    out = (((int32_t)data->coeffB[0]*(int32_t)data->w[0]) >> (data->bitRes -1))
+        + (((int32_t)data->coeffB[1]*(int32_t)data->w[1]) >> (data->bitRes -1))
+        + (((int32_t)data->coeffB[2]*(int32_t)data->w[2]) >> (data->bitRes -1));
     data->w[2] = data->w[1];
     data->w[1] = data->w[0];
-
+    sampleIn <<= (16 - data->bitRes);
     return (int16_t)(out & 0xFFFF);
 }
