854e55cc8a43ebb10510b4f4c32565b6444acc13
Philipp Fuerholz <fuerholz@gmx.ch>
2022-06-10 23:21:05 +0200

    simplified decimation filter

--- Src/common/audio/oversamplingWaveshaper.c
+++ Src/common/audio/oversamplingWaveshaper.c
@@ -38,19 +38,21 @@ void  applyOversamplingDistortion(uint16_t*data,OversamplingWaveshaperDataType* 
 }
 int16_t  OversamplingDistortionProcessSample(int16_t sample,OversamplingWaveshaperDataType* waveshaper)
 {
-    int16_t osVal1, osVal2, osVal3, osVal4;
+    int32_t osVal1, osVal2, osVal3, osVal4;
+    int16_t outVal;
     int16_t diff = sample - waveshaper->oldValue;
     osVal1 = waveshaper->oldValue + (diff >> 2); // old + 0.25*(new-old)
     osVal2 = waveshaper->oldValue + (diff >> 1); // old +0.5*(new-old)
     osVal3 = sample - (diff >> 2); // old + 0.75*(new-old)
     osVal4 = sample; // new
-
+    waveshaper->oldValue=sample;
     osVal1 = waveShaperProcessSample(osVal1,&waveshaper->waveshaper);
     osVal2 = waveShaperProcessSample(osVal2,&waveshaper->waveshaper);
     osVal3 = waveShaperProcessSample(osVal3,&waveshaper->waveshaper);
     osVal4 = waveShaperProcessSample(osVal4,&waveshaper->waveshaper);
 
-    osVal4 = secondOrderIirFilterProcessSample(osVal4,&waveshaper->oversamplingFilter);
-    return osVal4;
+    outVal = (int16_t)((osVal1+osVal2+osVal3+osVal4) >> 2);
+    //osVal4 = secondOrderIirFilterProcessSample(osVal4,&waveshaper->oversamplingFilter);
+    return outVal;
 }
 
