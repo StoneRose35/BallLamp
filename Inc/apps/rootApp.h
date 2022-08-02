6c1f50bb5e9dd8ec2ab0f5c2291e4b36677aa461
Philipp Fuerholz <fuerholz@gmx.ch>
2022-08-02 11:48:06 +0200

    split project, removing unnecessary content

--- Inc/apps/rootApp.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ROOT_APP_H_
-#define _ROOT_APP_H_
-#include "uiStack.h"
-
-void createRootApp(SubApplicationType* app,uint8_t index);
-#endif
--- Inc/apps/voidApp.h
+++
@@ -1,6 +0,0 @@
-#ifndef _VOID_APP_H_
-#define _VOID_APP_H_
-#include "uiStack.h"
-
-void createVoidApp(SubApplicationType* app,uint8_t index);
-#endif
