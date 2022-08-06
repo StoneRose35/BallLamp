b92285470b956f9426f24f29177d08c7f6d8755d
Philipp Fuerholz <fuerholz@gmx.ch>
2022-08-06 23:27:00 +0200

    first version of slave i2s driver

--- Src/rp2040/i2s.c
+++ Src/rp2040/i2s.c
@@ -180,6 +180,89 @@ void initI2S()
 	audioState = (1 << AUDIO_STATE_ON);
 }
 
+
+void initI2SSlave()
+{
+    uint16_t instr_mem_cnt = 0;
+	uint16_t first_instr_pos;
+
+	audioState = 0;
+ 
+    first_instr_pos = instr_mem_cnt;
+    // set wrap top and wrap bottom, set jump pin
+	*PIO1_SM0_EXECCTRL = 
+	( (i2s_duplex_slave_wrap_target + first_instr_pos) << PIO_SM0_EXECCTRL_WRAP_BOTTOM_LSB)
+	| ( (i2s_duplex_slave_wrap + first_instr_pos) << PIO_SM0_EXECCTRL_WRAP_TOP_LSB) | (I2S_WS_PIN << PIO_SM0_EXECCTRL_JMP_PIN_LSB);
+
+	// shift out to left and in from right 
+	*PIO1_SM0_SHIFTCTRL = (0 << PIO_SM0_SHIFTCTRL_PULL_THRESH_LSB) 
+                           | (0 << PIO_SM0_SHIFTCTRL_AUTOPULL_LSB) 
+                           | (0 << PIO_SM0_SHIFTCTRL_FJOIN_TX_LSB) 
+						   | (0 << PIO_SM0_SHIFTCTRL_OUT_SHIFTDIR_LSB)
+
+						   | (0 << PIO_SM0_SHIFTCTRL_PUSH_THRESH_LSB)
+						   | (0 << PIO_SM0_SHIFTCTRL_AUTOPUSH_LSB)
+						   | (0 << PIO_SM0_SHIFTCTRL_FJOIN_RX_LSB)
+						   | (0 << PIO_SM0_SHIFTCTRL_IN_SHIFTDIR_LSB)
+						   ;
+
+	// fill in instructions
+	// offset the jump instruction by position of the first command since the jump addresses
+	// are relative to the program
+	for(uint8_t c=0;c < i2s_duplex_slave_program.length;c++){
+		*(PIO1_INSTR_MEM + instr_mem_cnt++) = (*(i2s_duplex_slave_program.instructions + c) & 0xe000)==0 ?
+		 *(i2s_duplex_slave_program.instructions + c) + first_instr_pos : *(i2s_duplex_slave_program.instructions + c);
+	}
+
+    // 3 consecutive input pins: bclk, lrclk and data_in (from adc)
+	// one output pin: dout (to dac)
+	*PIO1_SM0_PINCTRL = 
+	(1 << PIO_SM0_PINCTRL_OUT_COUNT_LSB)
+    | (I2S_DATA_PIN << PIO_SM0_PINCTRL_OUT_BASE_LSB)
+	| (I2S_DATA_PIN << PIO_SM0_PINCTRL_SET_BASE_LSB)
+	| (I2S_DATA_IN_PIN << PIO_SM0_PINCTRL_IN_BASE_LSB)
+	| (1 << PIO_SM0_PINCTRL_SET_COUNT_LSB)
+    ;
+
+	// switch control of i2s pins to pio1
+	*I2S_BCK_PIN_CNTR = 7;
+    *I2S_DATA_PIN_CNTR = 7;
+    *I2S_WS_PIN_CNTR = 7;
+	*I2S_DATA_IN_PIN_CNTR = 7;
+
+	// set clock divider to 1 (fastest possible)
+	*PIO1_SM0_CLKDIV = (1 << PIO_SM0_CLKDIV_INT_LSB);
+
+    // set pin directions to output
+    *PIO1_SM0_INSTR = 0xE087;
+
+	// jump to first instruction
+	*PIO1_SM0_INSTR = first_instr_pos;
+
+	// initialize DMA
+	// dac output
+	*DMA_CH2_WRITE_ADDR = (uint32_t)PIO1_SM0_TXF;
+	dbfrPtr = 0;
+	*DMA_CH2_READ_ADDR = dbfrPtr + (uint32_t)i2sDoubleBuffer;
+	*DMA_CH2_TRANS_COUNT = AUDIO_BUFFER_SIZE;
+	*DMA_CH2_CTRL_TRIG = (8 << DMA_CH2_CTRL_TRIG_TREQ_SEL_LSB) 
+						| (1 << DMA_CH2_CTRL_TRIG_INCR_READ_LSB) 
+						| (2 << DMA_CH2_CTRL_TRIG_DATA_SIZE_LSB) // always read left and right at once
+						| (1 << DMA_CH2_CTRL_TRIG_EN_LSB);
+
+	// adc input
+	*DMA_CH3_READ_ADDR = (uint32_t)PIO1_SM0_RXF;
+	*DMA_CH3_WRITE_ADDR = dbfrInputPtr + (uint32_t)i2sDoubleBufferIn;
+	*DMA_CH3_TRANS_COUNT = AUDIO_BUFFER_SIZE;
+	*DMA_CH3_CTRL_TRIG = (12 << DMA_CH2_CTRL_TRIG_TREQ_SEL_LSB) // pio1 DREQ_PIO1_RX0
+						| (1 << DMA_CH3_CTRL_TRIG_INCR_WRITE_LSB) 
+						| (2 << DMA_CH3_CTRL_TRIG_DATA_SIZE_LSB) // always write left and right at once
+						| (1 << DMA_CH3_CTRL_TRIG_EN_LSB);
+
+	*DMA_INTE0 |= (1 << 3) | (1 << 2); // assert dma for channel 3 since the adc channel is always finished halb a bitclock cycle later
+
+}
+
 void toggleAudioBuffer()
 {
 	dbfrPtr += AUDIO_BUFFER_SIZE*4;
